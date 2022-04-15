5126f16f679f195131cd5e822626c8b20bcb69f4
Nicolás Silva <sovietfrance@gmail.com>
2022-04-15 15:10:13 -0400

    feat: update tests

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,8 +2,43 @@ pragma solidity ^0.5.0;
 
 contract Marketplace {
     string public name;
+    uint256 public productCount = 0;
+    mapping(uint256 => Product) public products;
+
+    struct Product {
+        uint256 id;
+        string name;
+        uint256 price;
+        address owner;
+        bool purchased;
+    }
+
+    event ProductCreated(
+        uint256 id,
+        string name,
+        uint256 price,
+        address owner,
+        bool purchased
+    );
 
     constructor() public {
         name = "Custom testing Marketplace";
+    }
+
+    function createProduct(string memory _name, uint256 _price) public {
+        require(bytes(_name).length > 0);
+        require(_price > 0);
+
+        productCount++;
+
+        products[productCount] = Product(
+            productCount,
+            _name,
+            _price,
+            msg.sender,
+            false
+        );
+        // Trigger an event
+        emit ProductCreated(productCount, _name, _price, msg.sender, false);
     }
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,25 +1,64 @@
-const Marketplace = artifacts.require('./Marketplace.sol')
+const Marketplace = artifacts.require("./Marketplace.sol");
 
-contract('Marketplace', (accounts) => {
-  let marketplace
+require("chai")
+  .use(require("chai-as-promised"))
+  .should();
+
+contract("Marketplace", ([deployer, seller, buyer]) => {
+  let marketplace;
 
   before(async () => {
-    marketplace = await Marketplace.deployed()
-  })
+    marketplace = await Marketplace.deployed();
+  });
 
-  describe('deployment', async () => {
-    it('deploys successfully', async () => {
-      const address = await marketplace.address
-      assert.notEqual(address, 0x0)
-      assert.notEqual(address, '')
-      assert.notEqual(address, null)
-      assert.notEqual(address, undefined)
-    })
+  describe("deployment", async () => {
+    it("deploys successfully", async () => {
+      const address = await marketplace.address;
+      assert.notEqual(address, 0x0);
+      assert.notEqual(address, "");
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+    });
 
-    it('has a name', async () => {
-      const name = await marketplace.name()
-      assert.equal(name, 'Custom testing Marketplace')
-    })
+    it("has a name", async () => {
+      const name = await marketplace.name();
+      assert.equal(name, "Custom testing Marketplace");
+    });
+  });
 
-  })
-})
+  describe("products", async () => {
+    let result, productCount;
+
+    before(async () => {
+      result = await marketplace.createProduct(
+        "iPhone X",
+        web3.utils.toWei("1", "Ether"),
+        { from: seller }
+      );
+      productCount = await marketplace.productCount();
+    });
+
+    it("creates products", async () => {
+      // SUCCESS
+      assert.equal(productCount, 1);
+      const event = result.logs[0].args;
+      assert.equal(
+        event.id.toNumber(),
+        productCount.toNumber(),
+        "id is correct"
+      );
+      assert.equal(event.name, "iPhone X", "name is correct");
+      assert.equal(event.price, "1000000000000000000", "price is correct");
+      assert.equal(event.owner, seller, "owner is correct");
+      assert.equal(event.purchased, false, "purchased is correct");
+
+      // FAILURE: Product must have a name
+      await marketplace.createProduct("", web3.utils.toWei("1", "Ether"), {
+        from: seller,
+      }).should.be.rejected;
+      // FAILURE: Product must have a price
+      await marketplace.createProduct("iPhone X", 0, { from: seller }).should.be
+        .rejected;
+    });
+  });
+});
